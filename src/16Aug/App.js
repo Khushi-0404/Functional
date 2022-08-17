058fcd7516d2e4ecdd108c232a0308f9ef9a2c18
Khushi-0404 <khushi.1@globallogic.com>
2022-08-17 12:34:37 +0530

    Counter App using redux

---
+++ src/16Aug/App.js
@@ -0,0 +1,13 @@
+
+import CounterConnector from "./React Redux Setup/CounterConnector"
+
+function App()
+{
+    return(
+        <div>
+     <CounterConnector></CounterConnector>
+       
+        </div>
+    )
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import ReactDOM from 'react-dom/client';
 //import App from './10Aug/App'
 //import App from './12Aug/Task2/App'
 //import App from './12Aug/Task1/App'
-import App from './12Aug/Task3/App'
-
+//import App from './12Aug/Task3/App'
+import App from './16Aug/App'
+import {createStore} from 'redux'
+import { Provider } from 'react-redux';
+import reducer from './16Aug/Reducer/index';
+const store=createStore(reducer)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+    <Provider store={store}>
     <App />
+</Provider>
+    //<App />
     //<FormHook></FormHook>
  
 );
